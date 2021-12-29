c729cbe761cea29885251a692f814f34961b109e
deberkemeier <deberkemeier@gmail.com>
2021-12-29 11:48:45 +0100

    change

--- src/app/pages/demo/demo.module.ts
+++ src/app/pages/demo/demo.module.ts
@@ -1,3 +1,16 @@
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -11,6 +24,20 @@ import { RouterModule } from '@angular/router';
     IonicModule,
     TranslateModule,
     RouterModule.forChild([{ path: '', component: DemoPage }]),
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatOptionModule
   ],
   declarations: [DemoPage]
 })
